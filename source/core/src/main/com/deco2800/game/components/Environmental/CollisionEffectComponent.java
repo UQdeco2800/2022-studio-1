6e582a3eb754b24e01b7b8de64ff1f02f45a07f4
Michelle Sands <michelle.a.sands@gmail.com>
2022-08-26 14:09:18 +1000

    added knockback and damage effects

--- source/core/src/main/com/deco2800/game/components/Environmental/CollisionEffectComponent.java
+++ source/core/src/main/com/deco2800/game/components/Environmental/CollisionEffectComponent.java
@@ -1,7 +1,9 @@
 package com.deco2800.game.components.Environmental;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Fixture;
+import com.deco2800.game.components.CombatStatsComponent;
 import com.deco2800.game.components.player.PlayerActions;
 import com.deco2800.game.entities.Entity;
 import com.deco2800.game.physics.BodyUserData;
@@ -15,7 +17,7 @@ import com.deco2800.game.physics.components.PhysicsMovementComponent;
  * class containing collision effects defined by the CollisionEffect enum
  * Entities implementing this must also implement ColliderComponent, PhysicsComponent & EnvironmentalComponent
  * Entities can have one collision effect only, as described by the enum
- * for area of effect, specify a hitboxcomponent and size
+ * collision effects impact both the player and npcs.
  */
 public class CollisionEffectComponent extends Component {
 
@@ -26,6 +28,7 @@ public class CollisionEffectComponent extends Component {
         DIVERT,
         SLOW,
         DAMAGE,
+        KNOCKBACK,
         NONE;
     }
 
@@ -33,8 +36,9 @@ public class CollisionEffectComponent extends Component {
     private ColliderComponent colliderComponent;
     private PhysicsComponent physicsComponent;
     private EnvironmentalComponent environmentalComponent;
-    private HitboxComponent hitboxComponent;
     private float speedModifier;
+    private float knockbackForce = 1f;
+    private int damage = 1;
 
     public CollisionEffectComponent(CollisionEffect collisionEffect) {
         this.collisionEffect = collisionEffect;
@@ -42,6 +46,8 @@ public class CollisionEffectComponent extends Component {
 
     /**
      * initialise the collisionEffect. Note this happens after entity has been attached
+     * damage and knockbackforce should be set separately, using setDamage() and setKnockbackForce()
+     * daage knocks back by default; set knockbackforce to zero to avoid this
      */
     @Override
     public void create() {
@@ -78,6 +84,14 @@ public class CollisionEffectComponent extends Component {
         return this;
     }
 
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setKnockbackForce(float knockbackForce) {
+        this.knockbackForce = knockbackForce;
+    }
+
     /**
      * Collision handler - adds effect on collision start
      * @param me the fixture associated with the ColliderComponent attached to this Entity
@@ -89,9 +103,9 @@ public class CollisionEffectComponent extends Component {
             //NB this could be changed to a different sized hitboxcomponent for AoE?
             return;
         }
+        Entity target = ((BodyUserData) other.getBody().getUserData()).entity;
         switch (this.getCollisionEffect()) {
             case SLOW:
-                Entity target = ((BodyUserData) other.getBody().getUserData()).entity;
                 PlayerActions playerActions = target.getComponent(PlayerActions.class);
                 PhysicsMovementComponent npcMovementComponent = target.getComponent(PhysicsMovementComponent.class);
                 if (playerActions != null) {
@@ -107,7 +121,24 @@ public class CollisionEffectComponent extends Component {
                     //this is working without using the setter method but maybe we should anyway?
                 } else {
                     break;
-            }
+                }
+            case DAMAGE:
+                CombatStatsComponent targetStats = target.getComponent(CombatStatsComponent.class);
+                if (targetStats != null) {
+                    CombatStatsComponent combatStats = new CombatStatsComponent(1, damage);
+                    targetStats.hit(combatStats);
+                    combatStats.dispose();
+                    //falls through to also knock back
+                }
+            case KNOCKBACK:
+                PhysicsComponent targetPhysicsComponent = target.getComponent(PhysicsComponent.class);
+                if (targetPhysicsComponent != null && knockbackForce > 0f) {
+                    Body targetBody = targetPhysicsComponent.getBody();
+                    Vector2 direction = target.getCenterPosition().sub(entity.getCenterPosition());
+                    Vector2 impulse = direction.setLength(knockbackForce);
+                    targetBody.applyLinearImpulse(impulse, targetBody.getWorldCenter(), true);
+                }
+                break;
             default:
                 break;
         }
