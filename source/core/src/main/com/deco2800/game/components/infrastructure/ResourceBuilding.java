bceb74b335904fc40e4258bb3fa9248543cb0c50
Cliff Worsfold <cliffworsfold@gmail.com>
2022-08-28 22:59:44 +1000

    Adds ResourceBuilding class

---
+++ source/core/src/main/com/deco2800/game/components/infrastructure/ResourceBuilding.java
@@ -0,0 +1,10 @@
+package com.deco2800.game.components.infrastructure;
+
+/**
+ * Resource building is a type of building used to generate resources for the player
+ */
+public class ResourceBuilding extends Infrastructure {
+    public ResourceBuilding(int health) {
+        super(health);
+    }
+}
