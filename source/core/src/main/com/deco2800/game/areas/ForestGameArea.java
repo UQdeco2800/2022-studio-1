e21bf4e35e98396d7e29b01e56d54e81ef07861e
mattbezett <mojbez1@gmail.com>
2022-08-28 22:33:13 +1000

    Changed active music file

--- source/core/src/main/com/deco2800/game/areas/ForestGameArea.java
+++ source/core/src/main/com/deco2800/game/areas/ForestGameArea.java
@@ -74,7 +74,7 @@ public class ForestGameArea extends GameArea {
       "images/terrain_iso_grass.atlas", "images/ghost.atlas", "images/ghostKing.atlas"
   };
   private static final String[] forestSounds = { "sounds/impact.mp3" };
-  private static final String backgroundMusic = "sounds/BGM_03_mp3.mp3";
+  private static final String backgroundMusic = "sounds/dusk.mp3";
 
   private static final String[] forestMusic = {backgroundMusic};
   private EnvironmentalCollision entityMapping;
