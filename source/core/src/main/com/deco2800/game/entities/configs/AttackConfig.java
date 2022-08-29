31d81b1f927afb0087706ca2b34a02630ee37f98
Rafe Klein <rafe171klein@gmail.com>
2022-08-29 13:57:27 +1000

    Added attack factory and config files

---
+++ source/core/src/main/com/deco2800/game/entities/configs/AttackConfig.java
@@ -0,0 +1,8 @@
+package com.deco2800.game.entities.configs;
+
+/**
+ * Defines all structure configs to be loaded by the Structure Factory.
+ */
+public class AttackConfig {
+  public BaseEntityConfig wall = new BaseEntityConfig();
+}
