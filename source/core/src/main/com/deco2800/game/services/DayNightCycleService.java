c66a2d1cc4dd1f345a700aafd1dcf7eac09addc3
Ella West <weste038@gmail.com>
2022-09-03 15:45:48 +1000

    Added JavaDoc comments to DayNightCycleService.java

--- source/core/src/main/com/deco2800/game/services/DayNightCycleService.java
+++ source/core/src/main/com/deco2800/game/services/DayNightCycleService.java
@@ -1,5 +1,8 @@
 package com.deco2800.game.services;
 
+/**
+ * Service for managing the Day and Night cycle of the game.
+ */
 public class DayNightCycleService {
     private boolean ended;
     //private CycleStatus currentCycleStatus;
@@ -8,7 +11,7 @@ public class DayNightCycleService {
 
     public DayNightCycleService(GameTime timer) {
         this.ended = false;
-        this.currentDayNumber = 0;
+        this.currentDayNumber = 1;
         this.currentDayMillis = timer.getTime();
     }
 
@@ -18,26 +21,50 @@ public class DayNightCycleService {
      * }
      */
 
+    /**
+     * Returns the current day number.
+     *
+     * @return int
+     */
     public int getCurrentDayNumber() {
         return this.currentDayNumber;
     }
 
+    /**
+     * Returns the current number of milliseconds since the current day night cycle started.
+     *
+     * @return long
+     */
     public long getCurrentDayMillis() {
         return this.currentDayMillis;
     }
 
+    /**
+     * Returns whether the current day night cycle has ended.
+     *
+     * @return boolean
+     */
     public boolean hasEnded() {
         return this.ended;
     }
 
+    /**
+     * Starts the day night cycle for the game.
+     */
     public void start() {
 
     }
 
+    /**
+     * Pauses the timer for the day night cycle.
+     */
     public void pause() {
 
     }
 
+    /**
+     * Main loop for the service that updates the game status.
+     */
     public void run() {
 
     }
