186b25ff5f6629140050e9d6ee6693f43ff34765
Lumin-cider <chengyu.lin1@uqconnect.edu.au>
2022-09-09 09:32:47 +1000

    "adding more junit tests to memento to test new equipments saving function, removing junit tests that are no longer relevant"

--- source/core/src/test/com/deco2800/game/memento/MementoTest.java
+++ source/core/src/test/com/deco2800/game/memento/MementoTest.java
@@ -2,6 +2,7 @@ package com.deco2800.game.memento;
 
 import com.deco2800.game.components.shop.artefacts.Artefact;
 import com.deco2800.game.components.shop.artefacts.BestSword;
+import com.deco2800.game.components.shop.equipments.Equipments;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -20,20 +21,23 @@ class MementoTest {
 
     @Test
     void shouldGetGold() {
-        Memento test = new Memento(1, 2, 3, 4,5, new ArrayList<>(), 5);
+        Memento test = new Memento(1, 2, 3, 4,5, new ArrayList<>(), 5, 6,
+                null, null, null);
         assertEquals(2, test.getGold());
     }
 
     @Test
     void shouldGetHealth() {
-        Memento test = new Memento(1, 2, 3, 4, 4, new ArrayList<>(), 5);
+        Memento test = new Memento(1, 2, 3, 4,5, new ArrayList<>(), 5, 6,
+                null, null, null);
         assertEquals(4, test.getCurrentHealth());
     }
 
     @Test
     void shouldGetEmptyItem() {
         List<Artefact> item = new ArrayList<>();
-        Memento test = new Memento(1, 2, 3, 4, 5, item, 5);
+        Memento test = new Memento(1, 2, 3, 4,5, item, 5, 6,
+                null, null, null);
         assertTrue(item.equals(test.getItemList()));
     }
 
@@ -41,20 +45,58 @@ class MementoTest {
     void shouldGetItemList() {
         List<Artefact> item = new ArrayList<>();
         item.add(new BestSword());
-        Memento test = new Memento(1, 2, 3, 4, 5, item, 5);
+        Memento test = new Memento(1, 2, 3, 4,5, item, 5, 6,
+                null, null, null);
         assertTrue(item.equals(test.getItemList()));
     }
 
     @Test
     void shouldGetAttack() {
-        Memento test = new Memento(1, 2, 3, 4, 5, new ArrayList<>(), 5);
+        Memento test = new Memento(1, 2, 3, 4,5, new ArrayList<>(), 5, 6,
+                null, null, null);
         assertEquals(5, test.getAttack());
     }
 
     @Test
     void shouldGetStone() {
-        Memento test = new Memento(1, 2, 3, 4, 5, new ArrayList<>(), 5);
+        Memento test = new Memento(1, 2, 3, 4,5, new ArrayList<>(), 5, 6,
+                null, null, null);
         assertEquals(3, test.getStone());
+    }
+
+    @Test
+    void shouldGetWood() {
+        Memento test = new Memento(1, 2, 3, 4,5, new ArrayList<>(), 5, 6,
+                null, null, null);
+        assertEquals(4, test.getWood());
+    }
+
+    @Test
+    void shouldGetDefense() {
+        Memento test = new Memento(1, 2, 3, 4,5, new ArrayList<>(), 5, 6,
+                null, null, null);
+        assertEquals(6, test.getDefense());
+    }
+
+    @Test
+    void shouldGetAxe() {
+        Memento test = new Memento(1, 2, 3, 4,5, new ArrayList<>(), 5, 6,
+                Equipments.AXE, null, null);
+        assertEquals(Equipments.AXE, test.getWeapon());
+    }
+
+    @Test
+    void shouldGetChestplate() {
+        Memento test = new Memento(1, 2, 3, 4,5, new ArrayList<>(), 5, 6,
+                null, Equipments.LV1_CHESTPLATE, null);
+        assertEquals(Equipments.LV1_CHESTPLATE, test.getChestplate());
+    }
+
+    @Test
+    void shouldGetHelmet() {
+        Memento test = new Memento(1, 2, 3, 4,5, new ArrayList<>(), 5, 6,
+                null, null, Equipments.LV1_HELMET);
+        assertEquals(Equipments.LV1_HELMET, test.getHelmet());
     }
 
 }
--- source/core/src/test/com/deco2800/game/memento/OriginatorTest.java
+++ source/core/src/test/com/deco2800/game/memento/OriginatorTest.java
@@ -1,6 +1,7 @@
 package com.deco2800.game.memento;
 import com.deco2800.game.components.shop.artefacts.Artefact;
 import com.deco2800.game.components.shop.artefacts.BestSword;
+import com.deco2800.game.components.shop.equipments.Equipments;
 import org.junit.jupiter.api.Test;
 import org.mockito.internal.matchers.Or;
 
@@ -65,5 +66,33 @@ class OriginatorTest {
         item.add(new BestSword());
         test.setItems(item);
         assertTrue(item.equals(test.getItemList()));
+    }
+
+    @Test
+    void shouldSaveWeapon() {
+        Originator test = new Originator(1);
+        test.setWeapon(Equipments.AXE);
+        assertEquals(Equipments.AXE, test.getWeapon());
+    }
+
+    @Test
+    void shouldSaveChestplate() {
+        Originator test = new Originator(1);
+        test.setChestplate(Equipments.LV1_CHESTPLATE);
+        assertEquals(Equipments.LV1_CHESTPLATE, test.getChestplate());
+    }
+
+    @Test
+    void shouldSaveHelmet() {
+        Originator test = new Originator(1);
+        test.setHelmet(Equipments.LV1_HELMET);
+        assertEquals(Equipments.LV1_HELMET, test.getHelmet());
+    }
+
+    @Test
+    void shouldSaveDefense() {
+        Originator test = new Originator(1);
+        test.setDefense(10);
+        assertEquals(10, test.getDefense());
     }
 }
