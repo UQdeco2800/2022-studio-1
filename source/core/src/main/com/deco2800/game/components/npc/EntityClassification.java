e7be84c044b91897e117a3b7c1ee85c2f151f689
Logan-De-Groot <logandegroot.9@gmail.com>
2022-09-10 22:36:27 +1000

    Comments and further bug fixes

--- source/core/src/main/com/deco2800/game/components/npc/EntityClassification.java
+++ source/core/src/main/com/deco2800/game/components/npc/EntityClassification.java
@@ -1,10 +1,16 @@
 package com.deco2800.game.components.npc;
 
 import com.deco2800.game.components.Component;
-import com.deco2800.game.entities.Entity;
 
+/**
+ * Classification System for NPC to be used by EffectsNearBY
+ * and other releated checking of entity types
+ */
 public class EntityClassification extends Component {
 
+    /**
+     * Types of entities
+     */
     public enum NPCClassification {
         NONE,
         BOSS,
@@ -14,14 +20,25 @@ public class EntityClassification extends Component {
 
     private NPCClassification entityType;
 
+    /**
+     * Constructor method for Entity Classification
+     * @param type the type of entity
+     */
     public EntityClassification(NPCClassification type) {
         this.entityType = type;
     }
 
+    /**
+     * Set entity type, useful for reassigning ENEMY to BOSS
+     * @param type NPCClassifcation to set
+     */
     public void setEntityType(NPCClassification type) {
         this.entityType = type;
     }
 
+    /**
+     * @return Returns the entity type
+     */
     public NPCClassification getEntityType() {
         return this.entityType;
     }
