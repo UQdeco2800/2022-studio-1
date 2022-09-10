a207fc33c560b9445a3d2b61270c60316fa9156f
Satya Bhasale <95593545+venndiagram5@users.noreply.github.com>
2022-09-11 00:03:47 +1000

    JSON version artefacts + equipment

--- source/core/src/main/com/deco2800/game/components/shop/equipments/Equipments.java
+++ source/core/src/main/com/deco2800/game/components/shop/equipments/Equipments.java
@@ -1,10 +1,14 @@
 package com.deco2800.game.components.shop.equipments;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Equipments {
     AXE,
     SWORD,
     BOW_AND_ARROW,
     TRIDENT,
+    SHIELD,
     LV1_CHESTPLATE,
     LV2_CHESTPLATE,
     LV3_CHESTPLATE,
@@ -12,13 +16,13 @@ public enum Equipments {
     LV2_HELMET,
     LV3_HELMET;
 
-
     public static String getFilepath(Equipments equipment) {
         return switch (equipment) {
-            case AXE -> "configs/equipments/DummyWeapon.json";
-            case SWORD -> "configs/equipments/DummyWeapon.json";
-            case BOW_AND_ARROW -> "configs/equipments/DummyWeapon.json";
-            case TRIDENT -> "configs/equipments/DummyWeapon.json";
+            case AXE -> "configs/equipments/axe.json";
+            case SWORD -> "configs/equipments/sword.json";
+            case BOW_AND_ARROW -> "configs/equipments/bowAndArrow.json";
+            case TRIDENT -> "configs/equipments/triton.json";
+            case SHIELD -> "configs/equipments/shield.json";
             case LV1_CHESTPLATE -> "configs/equipments/DummyArmor.json";
             case LV2_CHESTPLATE -> "configs/equipments/DummyArmor.json";
             case LV3_CHESTPLATE -> "configs/equipments/DummyArmor.json";
@@ -26,6 +30,11 @@ public enum Equipments {
             case LV2_HELMET -> "configs/equipments/DummyArmor.json";
             case LV3_HELMET -> "configs/equipments/DummyArmor.json";
         };
+    }
+
+    public static List<Equipments> getAllEquipmentTypes() {
+        List<Equipments> all = Arrays.asList(Equipments.class.getEnumConstants());
+        return all;
     }
 
 }
