5f2e818d736b9d9927c33ff42d15aeb32fd26fe6
Muhammad Zahran Agung Dewantoro <zahran.agung@gmail.com>
2022-09-12 17:13:33 +1000

    update setlevel test

--- source/core/src/test/com/deco2800/game/components/CombatStatsComponentTest.java
+++ source/core/src/test/com/deco2800/game/components/CombatStatsComponentTest.java
@@ -3,6 +3,8 @@ package com.deco2800.game.components;
 import com.deco2800.game.extensions.GameExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -55,14 +57,32 @@ class CombatStatsComponentTest {
 
   @Test
   void shouldSetLevel() {
-    CombatStatsComponent stats = new CombatStatsComponent(100, 0, 0,1);
-    assertEquals(1, stats.getLevel());
+      CombatStatsComponent stats = new CombatStatsComponent(100, 0, 0, 1);
+      assertEquals(1, stats.getLevel());
 
-    stats.setLevel(5);
-    assertEquals(5, stats.getLevel());
+      stats.setLevel(2);
+      assertEquals(2, stats.getLevel());
 
-    stats.setLevel(2);
-    assertEquals(2, stats.getLevel());
+      stats.setLevel(3);
+      assertEquals(3, stats.getLevel());
+
+      stats.setLevel(3);
+      assertEquals(3, stats.getLevel());
+
+      stats.setLevel(4);
+      assertEquals(4, stats.getLevel());
+
+      stats.setLevel(5);
+      assertEquals(5, stats.getLevel());
+
+      // Shouldn't be able set level 0 or less
+      stats.setLevel(0);
+      assertNotEquals(0, stats.getLevel());
+      assertFalse(stats.getLevel() < 1);
+
+      stats.setLevel(-1);
+      assertNotEquals(-1, stats.getLevel());
+      assertFalse(stats.getLevel() < 1);
   }
 
   @Test
@@ -95,8 +115,6 @@ class CombatStatsComponentTest {
 
     stats.setHealth(400);
     assertEquals(400, stats.getHealth());
-
-
   }
 
   @Test
