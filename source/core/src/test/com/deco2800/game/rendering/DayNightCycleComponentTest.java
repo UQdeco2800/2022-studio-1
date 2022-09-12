74e3615dd3a040bbee7ca5c587ade805191583fa
Chol Nhial <chol.nhial.chol@gmail.com>
2022-09-12 14:27:32 +1000

    create tests skeleton #128

---
+++ source/core/src/test/com/deco2800/game/rendering/DayNightCycleComponentTest.java
@@ -0,0 +1,14 @@
+package com.deco2800.game.rendering;
+
+import com.deco2800.game.extensions.GameExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(GameExtension.class)
+class DayNightCycleComponentTest {
+
+    @BeforeEach
+    void beforeEach() {
+
+    }
+}
