1ce8c0a995324d130728b30c0f177c417a914ed5
Logan-De-Groot <logandegroot.9@gmail.com>
2022-09-18 20:10:11 +1000

    Some serialisation methods tried. Custom serialisation methods are needed.

--- source/core/src/main/com/deco2800/game/files/SaveGame.java
+++ source/core/src/main/com/deco2800/game/files/SaveGame.java
@@ -1,9 +1,17 @@
 package com.deco2800.game.files;
 
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.utils.Json;
+import com.deco2800.game.components.Environmental.CollisionEffectComponent;
+import com.deco2800.game.components.Environmental.EnvironmentalComponent;
 import com.deco2800.game.entities.Entity;
 import com.deco2800.game.entities.EntityService;
 import com.deco2800.game.entities.factories.ObstacleFactory;
+import com.deco2800.game.physics.PhysicsLayer;
+import com.deco2800.game.physics.PhysicsUtils;
+import com.deco2800.game.physics.components.ColliderComponent;
+import com.deco2800.game.physics.components.PhysicsComponent;
+import com.deco2800.game.rendering.TextureRenderComponent;
 import com.deco2800.game.services.ServiceLocator;
 
 import java.io.File;
@@ -11,14 +19,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SaveGame {
-    private static String savePath = "Saves/1";
+    private static String savePath = "Saves/1.json";
+    //private Json
 
     private static void saveEntityService(int slotNumber) {
-        Entity tree = ObstacleFactory.createTree();
+        Entity environmentalObject = new Entity()
+                .addComponent(new EnvironmentalComponent().setObstacle(EnvironmentalComponent.EnvironmentalObstacle.TREE));
+                /*.addComponent(new CollisionEffectComponent(collisionEffect, speedModifier));
+        environmentalObject.setName("EnvironmentalObject");
+        environmentalObject.setCollectable(false);
+        environmentalObject.getComponent(PhysicsComponent.class).setBodyType(BodyDef.BodyType.StaticBody);
+        environmentalObject.getComponent(TextureRenderComponent.class).scaleEntity();
+        environmentalObject.scaleHeight(heightScale);
+        PhysicsUtils.setScaledCollider(environmentalObject, scaleX, scaleY);
+        return environmentalObject;*/
 
-        //Json json = new Json();
-        //System.out.println(json.prettyPrint(tree));
-        //FileLoader.writeClass(tree, savePath, FileLoader.Location.LOCAL);
+        FileLoader.writeClass(environmentalObject, savePath, FileLoader.Location.LOCAL);
+
+        Entity tree_return = FileLoader.readClass(Entity.class, savePath, FileLoader.Location.LOCAL);
+        System.out.println(tree_return.getCenterPosition());
+
     }
 
     public static void saveGameState(int slotNumber) {
