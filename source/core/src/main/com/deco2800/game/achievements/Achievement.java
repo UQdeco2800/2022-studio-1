0b2816703dce3ac467f76c93c75a35c81d46a962
Chol Nhial <chol.nhial.chol@gmail.com>
2022-09-21 19:32:05 +1000

    create core data classes for achievements in preparation for persistence #249

---
+++ source/core/src/main/com/deco2800/game/achievements/Achievement.java
@@ -0,0 +1,99 @@
+package com.deco2800.game.achievements;
+
+import java.util.Objects;
+
+
+/**
+ * A class that represents an achievement. It
+ * can be persisted.
+ */
+public class Achievement {
+
+    private int id;
+
+    private AchievementType achievementType;
+    private String name;
+    private String image;
+    private String description;
+    private int totalAchieved;
+    private boolean isCompleted;
+    private String achievementData; // CSV or JSON formatted data specific to this particular achievement
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTotalAchieved() {
+        return totalAchieved;
+    }
+
+    public void setTotalAchieved(int totalAchieved) {
+        this.totalAchieved = totalAchieved;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public String getAchievementData() {
+        return achievementData;
+    }
+
+    public void setAchievementData(String achievementData) {
+        this.achievementData = achievementData;
+    }
+
+    public AchievementType getAchievementType() {
+        return achievementType;
+    }
+
+    public void setAchievementType(AchievementType achievementType) {
+        this.achievementType = achievementType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Achievement that = (Achievement) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ source/core/src/main/com/deco2800/game/achievements/AchievementType.java
@@ -0,0 +1,14 @@
+package com.deco2800.game.achievements;
+
+/**
+ * Represents each type of achievement in the game
+ * each type can have many achievements
+ */
+public enum AchievementType {
+    RESOURCES,
+    BUILDINGS,
+    KILLS,
+    UPGRADES,
+    GAME,
+    MISC
+};
