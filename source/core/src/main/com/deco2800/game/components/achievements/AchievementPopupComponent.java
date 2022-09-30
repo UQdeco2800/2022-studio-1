bfaa4493bf76a2505b56becd21edb040d07011bc
Chol Nhial <chol.nhial.chol@gmail.com>
2022-09-30 22:12:12 +1000

    scaled UI assets for popup and popup for 5 seconds #267

--- source/core/src/main/com/deco2800/game/components/achievements/AchievementPopupComponent.java
+++ source/core/src/main/com/deco2800/game/components/achievements/AchievementPopupComponent.java
@@ -6,15 +6,24 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.deco2800.game.achievements.AchievementType;
+import com.deco2800.game.achievements.Achievement;
+import com.deco2800.game.services.AchievementHandler;
+import com.deco2800.game.services.ServiceLocator;
 import com.deco2800.game.ui.UIComponent;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class AchievementPopupComponent extends UIComponent {
 
+    public static final long POPUP_TIMEOUT = 5000;
     private Table content;
 
     private Table root;
     private Table details;
+
+    private Table background;
     private Image achievementTypeImage;
 
     private Image achievementPopupBackground;
@@ -23,63 +32,112 @@ public class AchievementPopupComponent extends UIComponent {
 
     private Label achievementDescriptionLabel;
 
+    private Queue<Achievement> popupQueue;
+
+    private long lastPopupTime;
+
+    private boolean isPopupActive;
+
+
+
     @Override
     public void create() {
         super.create();
-        addActors();
+        this.isPopupActive = false;
+        popupQueue = new LinkedList<>();
+        ServiceLocator.getAchievementHandler().getEvents()
+                .addListener(AchievementHandler.EVENT_STAT_ACHIEVEMENT_MADE, this::onAchievementMade);
+        ServiceLocator.getAchievementHandler().getEvents().addListener(AchievementHandler.EVENT_ACHIEVEMENT_MADE, this::onAchievementMade);
     }
 
+    private void onAchievementMade(Achievement achievement) {
+        this.popupQueue.add(achievement);
+    }
 
-    private void addActors() {
-
+    private void buildActors(String image, String title, String description) {
         root = new Table();
         root.setFillParent(true);
         root.debug();
-        root.padBottom(Gdx.graphics.getHeight() - 230);
+        root.debugActor();
+        root.debugCell();
+        root.padBottom(Gdx.graphics.getHeight() - 120);
 
         content = new Table();
         content.top();
-        content.setHeight(0.2f);
         content.debug();
 
 
+        background = new Table();
+        background.top();
+        background.debug();
+        background.setFillParent(true);
+
         details = new Table();
-        details.padBottom(10);
         details.setFillParent(true);
 
-        Texture achievementTypeTexture = new Texture(Gdx.files.internal(AchievementType.BUILDINGS.getImage()));
+        Texture achievementTypeTexture = new Texture(Gdx.files.internal(image));
         achievementTypeImage = new Image(achievementTypeTexture);
-        achievementTypeImage.setScale(0.5f);
 
-        Texture backgroundTexture = new Texture(Gdx.files.internal("images/achievements/Notification_Popup.png"));
+        Texture backgroundTexture = new Texture(Gdx.files.internal("images/achievements/Notification_Popup_scaled.png"));
         achievementPopupBackground = new Image(backgroundTexture);
-        //achievementPopupBackground.setScale(0.7f);
-        achievementPopupBackground.setHeight(0.2f);
-        achievementPopupBackground.pack();
-        var background = achievementPopupBackground.getDrawable();
-        content.setBackground(background);
-        content.pack();
+        background.add(achievementPopupBackground);
+        background.pack();
+
         content.add(achievementTypeImage);
-        achievementTitleLabel = new Label("Achievement Earnt: Upgrader", skin, "small");
-        achievementDescriptionLabel = new Label("Upgraded 5 times", skin, "small");
+        achievementTitleLabel = new Label("Achievement Earned: " + title, skin, "default");
+        achievementDescriptionLabel = new Label(description, skin, "small");
         details.add(achievementTitleLabel);
         details.row();
         details.add(achievementDescriptionLabel);
         content.add(details);
         root.add(content);
+    }
+
+    private void addActors() {
+        stage.addActor(background);
         stage.addActor(root);
+    }
+
+    private void clearActors() {
+        /* if root is not null than other aren't */
+        if (root != null) {
+            background.clear();
+            root.clear();
+            content.clear();
+            details.clear();
+        }
     }
 
     @Override
     public void dispose() {
-
         super.dispose();
+        clearActors();
     }
 
 
 
     @Override
     protected void draw(SpriteBatch batch) {
-        // handled by stage
+        if (!popupQueue.isEmpty() && !isPopupActive) {
+            var achievement = popupQueue.poll();
+            if (achievement.isStat()) {
+                clearActors();
+                buildActors(achievement.getAchievementType().getPopupImage(),
+                        achievement.getName(), achievement.getDescription().formatted(achievement.getTotalAchieved()));
+                addActors();
+                lastPopupTime = System.currentTimeMillis();
+                isPopupActive = true;
+            }
+        }
+
+        if(isPopupActive && shouldHidePopup()) {
+           clearActors();
+           isPopupActive = false;
+        }
+    }
+
+    private boolean shouldHidePopup() {
+        long currentTime = System.currentTimeMillis();
+        return currentTime - lastPopupTime >= POPUP_TIMEOUT;
     }
 }
