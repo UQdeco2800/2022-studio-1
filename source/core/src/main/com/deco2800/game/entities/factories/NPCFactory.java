acd26d2a33332b1e885b9e1c24e863165b88a9fc
LungYuLin <lungyu.lin@uqconnect.edu.au>
2022-10-04 10:10:46 +1000

    Add code to NPCFactory.java for making the starfish animated.

--- source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
@@ -204,13 +204,28 @@ public class NPCFactory {
     animator.addAnimation("float", 0.1f, Animation.PlayMode.LOOP);
     animator.addAnimation("angry_float", 0.1f, Animation.PlayMode.LOOP);
     */
+    AnimationRenderComponent animator =
+            new AnimationRenderComponent(
+                    ServiceLocator.getResourceService().getAsset("images/starfish_animation/starfish.atlas", TextureAtlas.class));
+    animator.addAnimation("fl", 0.1f, Animation.PlayMode.LOOP);
+    animator.addAnimation("fr", 0.1f, Animation.PlayMode.LOOP);
+    animator.addAnimation("bl", 0.1f, Animation.PlayMode.LOOP);
+    animator.addAnimation("br", 0.1f, Animation.PlayMode.LOOP);
+
+
     ninjaStarfish
             .addComponent(new CombatStatsComponent(config.health, config.baseAttack))
             .addComponent(new HealthBarComponent(100, 10))
             .addComponent(textureRenderComponent)
-            .addComponent(new DayNightCycleComponent());
+            .addComponent(new DayNightCycleComponent())
+            //.addComponent(textureRenderComponent);
+            .addComponent(new GhostAnimationController())
+            .addComponent(new EntityClassification(EntityClassification.NPCClassification.ENEMY));
 
-    ninjaStarfish.getComponent(TextureRenderComponent.class).scaleEntity();
+    ninjaStarfish.getComponent(AnimationRenderComponent.class).scaleEntity();
+    ServiceLocator.getEntityService().registerNamed("ninjaStarfish@" + ninjaStarfish.getId(), ninjaStarfish);
+    ninjaStarfish.setScale(1.2f, 1.2f);
+
     return ninjaStarfish;
   }
 
