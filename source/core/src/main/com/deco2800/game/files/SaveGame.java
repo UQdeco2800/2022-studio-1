592a6107930dd74d3b083cb4fffcb38f47707ab0
Logan-De-Groot <logandegroot.9@gmail.com>
2022-10-09 15:57:07 +1000

    Fixed mistake

--- source/core/src/main/com/deco2800/game/files/SaveGame.java
+++ source/core/src/main/com/deco2800/game/files/SaveGame.java
@@ -351,7 +351,6 @@ public class SaveGame {
         logger.debug("Begin Saving");
         try {
 
-            environmentalGenerationSetUp();
             saveEnvironmentalObjects();
 
             structureGenerationSetUp();
