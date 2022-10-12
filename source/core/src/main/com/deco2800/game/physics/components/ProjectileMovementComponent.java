c277cbdfcb2d49e21f5e9db053b35e81cd67bfab
Sam Behm <sambehmwork@gmail.com>
2022-10-13 07:20:59 +1000

    Added max travel distance for projectiles

--- source/core/src/main/com/deco2800/game/physics/components/ProjectileMovementComponent.java
+++ source/core/src/main/com/deco2800/game/physics/components/ProjectileMovementComponent.java
@@ -2,16 +2,26 @@ package com.deco2800.game.physics.components;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.deco2800.game.services.ServiceLocator;
 
 public class ProjectileMovementComponent extends PhysicsMovementComponent {
 
-        private Vector2 direction;
         private Vector2 origin;
+        private final float MAX_TRAVEL_DISTANCE = 500f;
 
         public void create() {
                 super.create();
                 physicsComponent.getBody().setBullet(true);
                 origin = entity.getPosition();
+        }
+
+        @Override
+        public void update() {
+                super.update();
+
+                if (Math.abs(getEntity().getPosition().dst(origin)) > MAX_TRAVEL_DISTANCE) {
+                        ServiceLocator.getUGSService().removeEntity(getEntity().getName());
+                }
         }
 
         @Override
@@ -26,7 +36,6 @@ public class ProjectileMovementComponent extends PhysicsMovementComponent {
         @Override
         protected void setToVelocity(Body body, Vector2 desiredVelocity) {
                 body.setLinearVelocity(desiredVelocity);
-                System.out.println("set velocity");
         }
 
 }
