abcb1a4008855652b6fe5b4b753472ce1a36e506
Chol Nhial <chol.nhial.chol@gmail.com>
2022-10-15 17:23:48 +1000

    remove debugging key #344

--- source/core/src/main/com/deco2800/game/rendering/DayNightCycleComponent.java
+++ source/core/src/main/com/deco2800/game/rendering/DayNightCycleComponent.java
@@ -1,7 +1,6 @@
 package com.deco2800.game.rendering;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
@@ -9,7 +8,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Vector3;
-import com.deco2800.game.input.InputComponent;
 import com.deco2800.game.services.DayNightCycleService;
 import com.deco2800.game.services.DayNightCycleStatus;
 import com.deco2800.game.services.ServiceLocator;
@@ -19,7 +17,7 @@ import org.slf4j.LoggerFactory;
 /**
  * This class is responsible for shading the game screen when invoked in RendererService
  */
-public class DayNightCycleComponent extends InputComponent {
+public class DayNightCycleComponent {
 
     Logger logger = LoggerFactory.getLogger(DayNightCycleComponent.class);
     private final ShaderProgram dayNightCycleShader;
@@ -38,11 +36,6 @@ public class DayNightCycleComponent extends InputComponent {
 
     private float intensity;
 
-    private boolean shouldRender;
-
-    private ShaderProgram defaultShader;
-
-
     /**
      * Creates a new DayNightCycleComponent. Making sure to compile shader files
      */
@@ -60,8 +53,7 @@ public class DayNightCycleComponent extends InputComponent {
         var vertexShader = Gdx.files.internal("shaders/base.vert");
         var lightShader=  Gdx.files.internal("shaders/light.frag");
         this.dayNightCycleShader = new ShaderProgram(vertexShader, lightShader);
-        shouldRender = true;
-        defaultShader = null;
+
         /* Subscribe to part of day changes */
         DayNightCycleService dayNightCycleService = ServiceLocator.getDayNightCycleService();
         if (dayNightCycleService != null) {
@@ -77,22 +69,14 @@ public class DayNightCycleComponent extends InputComponent {
      * @param batch the sprite batch to apply the filter to
      */
     public void render(SpriteBatch batch) {
-        // Get default shader on first call;
-        if (defaultShader == null) {
-            defaultShader = batch.getShader();
-        }
-        if (shouldRender) {
-            dayNightCycleShader.bind();
-            {
-                dayNightCycleShader.setUniformi("u_lightmap", 1);
-                dayNightCycleShader.setUniformf("ambientColor", ambientColour.x, ambientColour.y, ambientColour.z, intensity);
-                dayNightCycleShader.setUniformf("resolution", Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-                batch.setShader(dayNightCycleShader);
-                Texture tex = BLACK_IMAGE.getTexture();
-                tex.bind(0);
-            }
-        } else {
-            batch.setShader(defaultShader);
+        dayNightCycleShader.bind();
+        {
+            dayNightCycleShader.setUniformi("u_lightmap", 1);
+            dayNightCycleShader.setUniformf("ambientColor", ambientColour.x, ambientColour.y, ambientColour.z, intensity);
+            dayNightCycleShader.setUniformf("resolution", Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            batch.setShader(dayNightCycleShader);
+            Texture tex = BLACK_IMAGE.getTexture();
+            tex.bind(0);
         }
     }
 
@@ -115,24 +99,6 @@ public class DayNightCycleComponent extends InputComponent {
             case NIGHT -> dark;
             default -> bright;
         };
-    }
-
-    /**
-     * Toggles the shader on or off
-     *
-     * @param keycode one of the constants in {@link Input.Keys}
-     * @return whether to call again
-     */
-    public boolean keyUp (int keycode) {
-        if (keycode == Input.Keys.PERIOD) {
-            shouldRender = !shouldRender;
-            if (shouldRender) {
-                logger.info("Day Night Cycle VFX on!");
-            } else {
-                logger.info("Day Night Cycle VFX off!");
-            }
-        }
-        return true;
     }
 
     public ShaderProgram getDayNightCycleShader() {
