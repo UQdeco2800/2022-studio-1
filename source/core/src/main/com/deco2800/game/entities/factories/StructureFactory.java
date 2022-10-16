3471c1b7e6d249c2cb910473d5713de0c1245f85
jonooallen <jonooallen@gmail.com>
2022-10-17 02:49:40 +1000

    Fixing broken test

--- source/core/src/main/com/deco2800/game/entities/factories/StructureFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/StructureFactory.java
@@ -61,8 +61,13 @@ public class StructureFactory {
           "images/attack_towers/tow1_2_r.png"}, {"images/attack_towers/tow1_3_l.png",
           "images/attack_towers/tow1_3_r.png"} };
 
-  private static String [] tempTower2Sprites = {};
-  private static String[][] tower2Sprites = {{}, {}, {}};
+  //change to tower 2
+  private static String [] tempTower2Sprites = {"images/attack_towers/tempStructures/temp_tower3lv1Left.png",
+          "images/attack_towers/tempStructures/temp_tower3lv1Right.png"};
+  //Change to tower 2
+  private static String[][] tower2Sprites = {{"images/attack_towers/tower3lv1Left.png", "images/attack_tower3lv1Right.png"},
+          {"images/attack_towers/tower3lv2Left.png", "images/attack_tower3lv2Right.png"},
+          {"images/attack_towers/tower3lv3Left.png", "images/attack_tower3lv3Right.png"}};
   private static String[] tempTower3Sprites = {"images/attack_towers/tempStructures/temp_tower3lv1Left.png",
           "images/attack_towers/tempStructures/temp_tower3lv1Right.png"};
   private static String[][] tower3Sprites = {{"images/attack_towers/tower3lv1Left.png", "images/attack_tower3lv1Right.png"},
