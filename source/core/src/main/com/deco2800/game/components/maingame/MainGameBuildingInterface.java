6d07561495233d3c181a1476ccf7f4caaf7710e1
Olivialeighton <71741631+Olivialeighton@users.noreply.github.com>
2022-10-16 10:24:58 +1000

    fixed merge errors

--- source/core/src/main/com/deco2800/game/components/maingame/MainGameBuildingInterface.java
+++ source/core/src/main/com/deco2800/game/components/maingame/MainGameBuildingInterface.java
@@ -14,11 +14,8 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
-<<<<<<< HEAD
 import com.deco2800.game.areas.MainArea;
-=======
 import com.deco2800.game.areas.terrain.TerrainComponent;
->>>>>>> c146c9a03cfe1334d49888766f24d21769b242f5
 import com.deco2800.game.components.CombatStatsComponent;
 import com.deco2800.game.components.player.InventoryComponent;
 import com.deco2800.game.components.player.PlayerStatsDisplay;
