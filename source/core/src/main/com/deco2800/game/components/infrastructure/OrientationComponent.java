914951a5ef2a4cafd84db5532f8d16b2dc628873
jonooallen <jonooallen@gmail.com>
2022-10-17 01:15:05 +1000

    Add javadoc to orientation component

--- source/core/src/main/com/deco2800/game/components/infrastructure/OrientationComponent.java
+++ source/core/src/main/com/deco2800/game/components/infrastructure/OrientationComponent.java
@@ -7,14 +7,27 @@ public class OrientationComponent extends Component {
 
         private int orientation = 0;
 
+        /**
+         * get the orientation of the entity
+         * @return orientation of the entity 0 or 1
+         */
         public int getOrientation() {
                 return orientation;
         }
 
+        /**
+         * set the orientation of a structure
+         * @param orientation of a structure 0 or 1
+         */
         public void setOrientation(int orientation) {
                 this.orientation = orientation;
         }
 
+        /**
+         * orientation component constructor
+         * initializes entity orientation
+         * @param orientation of the entity 0 or 1
+         */
         public OrientationComponent(int orientation) {
                 this.orientation = orientation;
         }
