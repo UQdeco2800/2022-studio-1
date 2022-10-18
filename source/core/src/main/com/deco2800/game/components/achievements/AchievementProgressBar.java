663a9d97189457c23b83f1bc4db5d620db71bdcc
Chol Nhial <chol.nhial.chol@gmail.com>
2022-10-18 12:22:07 +1000

    Create a ProgressBar UI element for achievements add to summary page and individual achievement pages - extra fix bug causing save file to be overwritten #224

--- source/core/src/main/com/deco2800/game/components/achievements/AchievementProgressBar.java
+++ source/core/src/main/com/deco2800/game/components/achievements/AchievementProgressBar.java
@@ -1,19 +1,19 @@
 package com.deco2800.game.components.achievements;
 
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.deco2800.game.achievements.AchievementType;
 import com.deco2800.game.areas.ForestGameArea;
-import com.deco2800.game.components.CombatStatsComponent;
 import com.deco2800.game.ui.UIComponent;
 import com.deco2800.game.utils.DrawableUtil;
-import com.deco2800.game.utils.RenderUtil;
 
+/**
+ * Progressbar used to show completion of achievements
+ */
 public class AchievementProgressBar extends UIComponent {
     private final ProgressBar progressBar;
 
@@ -23,11 +23,17 @@ public class AchievementProgressBar extends UIComponent {
 
     private boolean showPercentage;
 
+
+    private boolean hideLabel;
+
+    private AchievementType achievementType;
+
     Table progressBarContent;
 
     Label label;
 
-    public AchievementProgressBar(int width, int height, int max, boolean showPercentage) {
+    public AchievementProgressBar(int width, int height, int max, boolean showPercentage, AchievementType type) {
+        this.achievementType = type;
         this.progressBar = new ProgressBar (0f,
                 1f,
                 0.01f,
@@ -49,8 +55,8 @@ public class AchievementProgressBar extends UIComponent {
 
         this.showPercentage = showPercentage;
 
+        this.hideLabel = false;
         this.target = max;
-        this.buildActor();;
     }
 
     private void updateLabel() {
@@ -70,28 +76,46 @@ public class AchievementProgressBar extends UIComponent {
         }
     }
 
-    private void buildActor() {
-        this.progressBarContent = new Table();
-        this.progressBarContent.add(progressBar);
-        progressBarContent.row();
-        updateLabel();
-        progressBarContent.add(label);
-        progressBarContent.setDebug(true);
+    public void buildActor() {
+        if (!hideLabel) {
+            this.progressBarContent = new Table();
+            this.progressBarContent.add(progressBar);
+            progressBarContent.row();
+            updateLabel();
+            progressBarContent.add(label);
+        }
+
     }
 
     public Actor getActor() {
-        return this.progressBarContent;
+        if (!hideLabel) {
+            if (progressBarContent == null) {
+                buildActor();
+            }
+            return this.progressBarContent;
+        }
+        return this.progressBar;
     }
 
     public void setDone(int amountDone) {
         this.done = amountDone;
         this.progressBar.setValue( (float) this.done/this.target);
         this.progressBar.updateVisualValue();
-        updateLabel();
+        if (!hideLabel) {
+            updateLabel();
+        }
     }
 
     @Override
     protected void draw(SpriteBatch batch) {
         // do nothing
+    }
+
+    public void setHideLabel(boolean hideLabel) {
+        this.hideLabel = hideLabel;
+    }
+
+    public AchievementType getAchievementType() {
+        return achievementType;
     }
 }
